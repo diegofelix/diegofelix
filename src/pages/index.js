ca5b1dfff04526c2211663145557bcfd0909e613
Diego Felix <diegoflx.oliveira@gmail.com>
2019-07-13 22:37:31 -0300

    Basic layout template for new website

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hello world!</h1>
+    <p>I'm working on my new website!</p>
+    <p>Please, follow me on twitter <a href="https://twitter.com/diegofelix">@diegofelix</a> and github <a href="https://github.com/diegofelix">@diegofelix</a></p>
+
+    <p>See you there!</p>
   </Layout>
 )
 
